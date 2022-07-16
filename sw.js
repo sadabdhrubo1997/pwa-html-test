443ebfc04379ae5c5262a5ade4c7dd95574ac6d4
sadabdhrubo1997 <sadabdhrubo@gmail.com>
2022-07-16 21:21:02 +0600

    added

--- sw.js
+++ sw.js
@@ -4,14 +4,14 @@ self.addEventListener('install', (e) => {
       .open('fox-store')
       .then((cache) =>
         cache.addAll([
-          '/',
-          '/index.html',
-          '/index.js',
-          '/style.css',
-          '/images/fox1.jpg',
-          '/images/fox2.jpg',
-          '/images/fox3.jpg',
-          '/images/fox4.jpg',
+          '/pwa-html-test/',
+          '/pwa-html-test/index.html',
+          '/pwa-html-test/index.js',
+          '/pwa-html-test/style.css',
+          '/pwa-html-test/images/fox1.jpg',
+          '/pwa-html-test/images/fox2.jpg',
+          '/pwa-html-test/images/fox3.jpg',
+          '/pwa-html-test/images/fox4.jpg',
         ])
       )
   );
